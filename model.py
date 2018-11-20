8ac5cf9a24551aed293bef32446fd4ebcbc382ee
bosung <bosung.kim3416@gmail.com>
2018-11-20 19:49:50 +0900

    LSTM + max pooling

--- model.py
+++ model.py
@@ -16,9 +16,8 @@ class SentenceEncoder(nn.Module):
         else:
             self.word_embedding = nn.Embedding.from_pretrained(embeddings, freeze=False)
 
-        self.lstm = nn.LSTM(word_dim, self.q_hidden_size, num_layers=num_layers, batch_first=True,
-                            bidirectional=True)
-        self.last = nn.Linear(self.q_hidden_size * 2, 2)
+        self.lstm = nn.LSTMCell(word_dim, self.q_hidden_size)
+        self.last = nn.Linear(self.q_hidden_size * 4, 2)
         self.softmax = nn.LogSoftmax(dim=1)
         self.sigmoid = nn.Sigmoid()
 
@@ -30,28 +29,47 @@ class SentenceEncoder(nn.Module):
         q_embedded = self.word_embedding(question)
         s_embedded = self.word_embedding(sentence)
 
-        _, (hq, cn) = self.lstm(q_embedded)
-        _, (hs, cn) = self.lstm(s_embedded)
-        hq1, hq2, hs1, hs2 = hq[0], hq[1], hs[0], hs[1]
-        sub = hq1 + (-hs1)
-        mul = torch.mul(hq1, hs1)
-        out = self.last(torch.cat((torch.cat((hq1, hs1), dim=1),
-                                   torch.cat((sub, mul), dim=1)), dim=1))
+        # max pooling
+        hq = self.max_pooling(q_embedded)
+        hs = self.max_pooling(s_embedded)
+
+        # sub = hq + (-hs)
+        # mul = torch.mul(hq, hs)
+        # out = self.last(torch.cat((torch.cat((hq, hs), dim=1),
+        #                            torch.cat((sub, mul), dim=1)), dim=1))
         # return self.softmax(out)  # Logsoftmax with NLLLoss
+
+        out = self.last(torch.cat((hq, hs), dim=1))
         return self.sigmoid(out)  # sigmoid with BCELoss
+
+    def max_pooling(self, _input):
+        vector = _input.transpose(1, 0)
+        # vector -> (max_length, batch_size, input_size)
+
+        hx, cx = self.lstm(vector[0])
+        output = hx.unsqueeze(1)
+        # hx -> (batch, 1, hidden_size)
+        for i in range(1, vector.size(0)):
+            hx, cx = self.lstm(vector[i], (hx, cx))
+            output = torch.cat((output, hx.unsqueeze(1)), dim=1)
+
+        return torch.max(output, dim=1)[0]
 
     def sent_embed(self, _input):
         embedded = self.word_embedding(_input)
-
+        """
         # 1. encoder last hidden
         _, (hq, cn) = self.lstm(embedded)
         _f, _b = hq
         return torch.cat((_f, _b), dim=1)
-        """
+
         # 2. word embedding avg
         hq = torch.mean(embedded, dim=1).squeeze(1)
         return hq
         """
+
+        # 3. max pooling
+        return self.max_pooling(embedded)
 
     def init_hidden(self, batch_size=None):
         if batch_size is None:
